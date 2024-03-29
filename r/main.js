e27ed2db4d828dcdc37cd717d37b5d92968cd15a
Zaz Brown <zazbrown@zazbrown.com>
2024-03-29 18:34:17 +0000

    make pages load instantly

--- r/main.js
+++ r/main.js
@@ -1,11 +1,40 @@
 const de_obfuscate = (_, email) =>
 	email.replace("...", "@").replace(/\.\.\./g, ".")
 
-$(document).ready( () =>
+const init = () => {
 	$(".eml").each(function() {
 		$(this).html(de_obfuscate)
 		if ($(this).attr("href")) {
 			return $(this).attr("href", de_obfuscate)
 		}
 	})
-)
+
+
+	$('body').on('click', 'a.instant', load_in_place)
+	$('body').on('click', 'header a.instant', load_in_place) // this is a mystery
+}
+
+
+const load_in_place = function() {
+	let href = $(this).attr('href');
+	$('main').load(href + ' main', (response, status, xhr) => {
+		if (status == 'success') {
+			// history.pushState(null, null, href)
+			window.location.href = href
+			$(init)
+		}
+	})
+	return false
+}
+
+$(window).bind('popstate', function() {
+	let href = location.pathname
+	$('main').load(href + ' main', (response, status, xhr) => {
+		if (status == 'success') {
+			$(init)
+		}
+	})
+})
+
+
+$(document).ready( init )
